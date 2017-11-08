c5c2bc3e1486501b23fb0a093adc98d107da8dd1
Jeremy Branham <jdbranham@hotmail.com>
2017-11-08 09:46:55 -0600

    refactor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,20 +3,32 @@ import { SecurityModule, SecurityMockService, ISecurityService } from '@savantly
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideRoutes } from '@angular/router';
-import { MenuModule, MenuService, menuServiceFactory } from '@savantly/ngx-menu';
+import { MenuModule, MenuService, MenuComponent } from '@savantly/ngx-menu';
 
+import { MatMenuModule, MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {APP_BASE_HREF} from '@angular/common';
+import { AppModule } from './app.module';
+
+export const menuServiceFactory = function(securityService: ISecurityService){
+  return new MenuService(securityService);
+};
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, MenuModule],
-      declarations: [
-        AppComponent
+      imports: [
+        RouterTestingModule, MatMenuModule, MatToolbarModule, MatButtonModule, FlexLayoutModule,
+        //AppModule,
+        //SecurityModule.forRoot(new SecurityMockService()),
+        MenuModule
       ],
+      declarations: [AppComponent],
       providers: [
         provideRoutes([]),
+        {provide: APP_BASE_HREF, useValue: '/'},
         {provide: ISecurityService, useClass: SecurityMockService},
-        {provide: MenuService, useFactory: menuServiceFactory, deps: [ISecurityService]}]
+        {provide: MenuService, useFactory: menuServiceFactory, deps: [ISecurityService]}],
     }).compileComponents();
   }));
 
